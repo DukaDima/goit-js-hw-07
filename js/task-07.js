b8d191401bb57b26f222099496cdd8c561c1b5ab
Dima <75967292+DukaDima@users.noreply.github.com>
2021-05-21 21:22:50 +0300

    Оптимизирует 7ю задачу

--- js/task-07.js
+++ js/task-07.js
@@ -3,12 +3,15 @@ const text = document.getElementById("text")
 
 console.log(range);
 console.log(text);
-
+// Диапазон и шаг
 range.min = 20
 range.max = 100
 range.step = 1
+// Начальные значения
+range.value = 60
+text.style.fontSize =range.value+"px"
 
-
+// Динамические значения
 range.onchange = function() {
     console.log(this.value)
   text.style.fontSize = this.value+"px"
